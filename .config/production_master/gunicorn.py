82a9109264f2ec61264d79f494540f9d79751243
LeeHanYeong <dev@lhy.kr>
2019-06-29 19:07:19 +0900

    gunicorn설정 fix

--- .config/production_master/gunicorn.py
+++ .config/production_master/gunicorn.py
@@ -8,5 +8,5 @@ accesslog = '/var/log/gunicorn/access.log'
 errorlog = '/var/log/gunicorn/error.log'
 capture_output = True
 raw_env = [
-    'DJANGO_SETTINGS_MODULE=config.settings.production_production',
+    'DJANGO_SETTINGS_MODULE=config.settings.production_master',
 ]
