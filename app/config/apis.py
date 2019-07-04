bbc6ac27820b6f360347775a4b6db6b94ba531b3
LeeHanYeong <dev@lhy.kr>
2019-07-05 03:49:56 +0900

    FrontDeploy APIView Response내용 추가

--- app/config/apis.py
+++ app/config/apis.py
@@ -13,18 +13,30 @@ class FrontDeployAPIView(APIView):
 
     def post(self, request):
         os.chdir(FRONT_DIR)
-        result_pull = run('git pull', stdout=PIPE, stderr=PIPE, shell=True)
-        result_restart = run('pm2 restart Yuni-Q_letusgo', stdout=PIPE, stderr=PIPE, shell=True)
+        result_git_pull = run('git pull', stdout=PIPE, stderr=PIPE, shell=True)
+        result_git_last_commit = run('git rev-parse HEAD', stdout=PIPE, stderr=PIPE, shell=True)
+        result_pm2_restart = run('pm2 restart Yuni-Q_letusgo', stdout=PIPE, stderr=PIPE, shell=True)
+        result_pm2_list = run('pm2 list', stdout=PIPE, stderr=PIPE, shell=True)
         data = {
-            'pull': {
-                'returncode': result_pull.returncode,
-                'stdout': result_pull.stdout,
-                'stderr': result_pull.stderr,
+            'git_pull': {
+                'returncode': result_git_pull.returncode,
+                'stdout': result_git_pull.stdout.decode('utf-8'),
+                'stderr': result_git_pull.stderr.decode('utf-8'),
             },
-            'restart': {
-                'returncode': result_restart.returncode,
-                'stdout': result_restart.stdout,
-                'stderr': result_restart.stderr,
+            'git_last_commit': {
+                'returncode': result_git_last_commit.returncode,
+                'stdout': result_git_last_commit.stdout.decode('utf-8'),
+                'stderr': result_git_last_commit.stderr.decode('utf-8'),
+            },
+            'pm2_restart': {
+                'returncode': result_pm2_restart.returncode,
+                'stdout': result_pm2_restart.stdout.decode('utf-8'),
+                'stderr': result_pm2_restart.stderr.decode('utf-8'),
+            },
+            'pm2_list': {
+                'returncode': result_pm2_list.returncode,
+                'stdout': result_pm2_list.stdout.decode('utf-8'),
+                'stderr': result_pm2_list.stderr.decode('utf-8'),
             },
         }
         return Response(data)
