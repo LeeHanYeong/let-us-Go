3abf4623ca9b442b0d521b99c7a8ae4b2dffce10
이한영 <lhy@tpay.co.kr>
2019-07-05 15:55:20 +0900

    FrontDeploy에 npm run build command추가

--- app/config/apis.py
+++ app/config/apis.py
@@ -1,10 +1,10 @@
 import os
 from subprocess import run, PIPE
 
+from drf_yasg.openapi import Response as APIResponse
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
-from drf_yasg.openapi import Response as APIResponse
 from rest_framework.views import APIView
 from rest_framework_api_key.permissions import HasAPIKey
 
@@ -26,6 +26,7 @@ class FrontDeployAPIView(APIView):
     서버에서 차례대로 아래 명령어들을 실행한다
     - `git pull`
     - `git rev-parse HEAD`
+    - `npm run build`
     - `pm2 restart Yuni-Q_letusgo`
     - `pm2 list`
 
@@ -51,31 +52,45 @@ class FrontDeployAPIView(APIView):
         }
     )
     def post(self, request):
+        class Subprocess:
+            OPTIONS = {
+                'stdout': PIPE,
+                'stderr': PIPE,
+                'shell': True,
+            }
+
+            def __init__(self, name, command, options=None):
+                self.name = name
+                self.command = command
+                self.options = self.OPTIONS.copy()
+                if options:
+                    self.options.update(**options)
+                self._result = None
+
+            def run(self):
+                self._result = run(self.command, **self.options)
+                return self._result
+
+            @property
+            def result(self):
+                if not self._result:
+                    self.run()
+                return self._result
+
         os.chdir(FRONT_DIR)
-        result_git_pull = run('git pull', stdout=PIPE, stderr=PIPE, shell=True)
-        result_git_last_commit = run('git rev-parse HEAD', stdout=PIPE, stderr=PIPE, shell=True)
-        result_pm2_restart = run('pm2 restart Yuni-Q_letusgo', stdout=PIPE, stderr=PIPE, shell=True)
-        result_pm2_list = run('pm2 list', stdout=PIPE, stderr=PIPE, shell=True)
+        subprocess_list = (
+            Subprocess('git_pull', 'git pull'),
+            Subprocess('git_last_commit', 'git rev-parse HEAD'),
+            Subprocess('npm_build', 'npm run build'),
+            Subprocess('pm2_restart', 'pm2 restart Yuni-Q_letusgo'),
+            Subprocess('pm2_list', 'pm2 list'),
+        )
+        results = {command.name: command.result for command in subprocess_list}
         data = {
-            'git_pull': {
-                'returncode': result_git_pull.returncode,
-                'stdout': result_git_pull.stdout.decode('utf-8'),
-                'stderr': result_git_pull.stderr.decode('utf-8'),
-            },
-            'git_last_commit': {
-                'returncode': result_git_last_commit.returncode,
-                'stdout': result_git_last_commit.stdout.decode('utf-8'),
-                'stderr': result_git_last_commit.stderr.decode('utf-8'),
-            },
-            'pm2_restart': {
-                'returncode': result_pm2_restart.returncode,
-                'stdout': result_pm2_restart.stdout.decode('utf-8'),
-                'stderr': result_pm2_restart.stderr.decode('utf-8'),
-            },
-            'pm2_list': {
-                'returncode': result_pm2_list.returncode,
-                'stdout': result_pm2_list.stdout.decode('utf-8'),
-                'stderr': result_pm2_list.stderr.decode('utf-8'),
-            },
+            key: {
+                'returncode': result.returncode,
+                'stdout': result.stdout.decode('utf-8'),
+                'stderr': result.stderr.decode('utf-8'),
+            } for key, result in results.items(),
         }
         return Response(data)
