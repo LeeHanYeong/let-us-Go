4517d9916f8966d49dcdbf7a0c3cfcdb9b6d29ad
LeeHanYeong <dev@lhy.kr>
2019-07-12 02:18:39 +0900

    Authorization문서 작성

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -77,8 +77,25 @@ REST_FRAMEWORK = {
         'no_underscore_before_number': True,
     },
     'EXCEPTION_HANDLER': 'utils.drf.exceptions.custom_exception_handler',
-    # 'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler'
+}
 
+# drf-yasg
+TOKEN_DESCRIPTION = '''
+### [DRF AuthToken](https://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication)
+인증정보를 사용해 [AuthToken](#operation/auth_token_create) API에 요청, 결과로 돌아온 **key**를  
+HTTP Request의 Header `Authorization`에 `Token <key>`값을 넣어 전송
+
+```
+Authorization: Token fs8943eu342cf79d8933jkd
+``` 
+'''
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Token': {
+            'type': 'DRF AuthToken',
+            'description': TOKEN_DESCRIPTION,
+        }
+    }
 }
 
 # AWS
