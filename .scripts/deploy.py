4458dfd3ed9b819878c38f2453bc2dbb87a33a96
LeeHanYeong <dev@lhy.kr>
2019-08-05 17:07:19 +0900

    create/deploy/swap/terminate 한 번에 실행하는 script추가

--- .scripts/deploy.py
+++ .scripts/deploy.py
@@ -4,9 +4,11 @@ import json
 import os
 import shutil
 import subprocess
+from time import sleep
+
+import boto3
 
 parser = argparse.ArgumentParser()
-parser.add_argument('env', type=str)
 parser.add_argument('--build', action='store_true')
 parser.add_argument('--run', action='store_true')
 parser.add_argument('--bash', action='store_true')
@@ -22,13 +24,22 @@ SECRETS = json.load(open(os.path.join(SECRETS_DIR, 'base.json')))
 
 ACCESS_KEY = SECRETS['AWS_EB_ACCESS_KEY_ID']
 SECRET_KEY = SECRETS['AWS_EB_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID'] = ACCESS_KEY
+os.environ['AWS_SECRET_ACCESS_KEY'] = SECRET_KEY
 ENV = dict(os.environ, AWS_ACCESS_KEY_ID=ACCESS_KEY, AWS_SECRET_ACCESS_KEY=SECRET_KEY)
 ENV['PYTHONPATH'] = ENV.get('PYENV_VIRTUAL_ENV') or ENV.get('VIRTUAL_ENV')
 
+# Docker Images
 IMAGE_BASE = 'azelf/letusgo:base'
 IMAGE_PRODUCTION_LOCAL = 'letusgo'
 IMAGE_PRODUCTION_ECR = '347809506342.dkr.ecr.ap-northeast-2.amazonaws.com/letusgo:app'
 
+# EB Variables
+ENV_NAME_BLUE = 'letusgo-blue'
+ENV_NAME_GREEN = 'letusgo-green'
+CNAME_PREFIX = 'letusgo.'
+
+# Docker commands
 RUN_OPTIONS = (
     '-p 8000:80',
     '--name letusgo',
@@ -46,7 +57,7 @@ def run(cmd, **kwargs):
     subprocess.run(cmd, shell=True, env=ENV, **kwargs)
 
 
-if __name__ == '__main__':
+def build():
     os.chdir(ROOT_DIR)
     os.makedirs(os.path.join(ROOT_DIR, '.temp'), exist_ok=True)
 
@@ -82,30 +93,124 @@ if __name__ == '__main__':
     run('DJANGO_SETTINGS_MODULE=config.settings.production_master python app/manage.py migrate --noinput')
     os.chdir(os.path.join(ROOT_DIR))
 
-    if args.build or args.run or args.bash:
-        run(f'docker build -t {IMAGE_PRODUCTION_LOCAL} -f Dockerfile .')
-        if args.build:
-            exit(0)
+    # Build local image
+    run(f'docker build -t {IMAGE_PRODUCTION_LOCAL} -f Dockerfile .')
+
+
+if __name__ == '__main__':
+    os.chdir(ROOT_DIR)
+    # 로컬 이미지 빌드
+    build()
+
+    # build/run/bash옵션 처리
+    if args.build:
+        exit(0)
 
     if args.run:
         run(f'{RUN_CMD}')
         exit(0)
+
     if args.bash:
         run(f'{RUN_CMD} /bin/bash')
         exit(0)
 
-    # Push BaseImage
+    # 위 3경우 외에는 deploy이므로, 이미지 push후 진행
     run(f'docker push {IMAGE_BASE}')
 
-    # Push ECR
-    # run(f'docker build -t {IMAGE_PRODUCTION_LOCAL} -f Dockerfile.production .')
-    # run(f'docker tag {IMAGE_PRODUCTION_LOCAL} {IMAGE_PRODUCTION_ECR}')
-    # run(f'$(aws ecr get-login --no-include-email --region ap-northeast-2) && '
-    #     f'docker push {IMAGE_PRODUCTION_ECR}')
+    # AWS Clients
+    client = boto3.client('elasticbeanstalk')
+    elb_client = boto3.client('elbv2')
+    ec2_client = boto3.client('ec2')
 
+    # 실행중인 Environment가져오기
+    environments = [
+        environment for environment in client.describe_environments()['Environments']
+        if environment['Status'] != 'Terminated'
+    ]
+    if len(environments) != 1:
+        raise Exception(f'실행중인 환경이 1개가 아닙니다 (총 {len(environments)}개)')
+    environment = environments[0]
+
+    # 실행중인 Environment의 Name, 새로 생성해서 Swap할 Environment의 Name
+    environment_name = environment['EnvironmentName']
+    swap_environment_name = ENV_NAME_GREEN if environment_name == ENV_NAME_BLUE else ENV_NAME_BLUE
+    print(f'실행중인 Environment: {environment_name}')
+    print(f'새로 생성할 Environment: {swap_environment_name}')
+
+    # eb create
+    run(f'eb create {swap_environment_name} --cname letusgo-swap --elb-type application --sample')
+
+    # staged영역 포함한 eb deploy실행
     run('git add -A')
     run('git add -f .master')
     run('git add -f .secrets')
-    run(f'eb deploy --staged --timeout 20 {args.env} &')
-    run('sleep 10')
+    run(f'eb deploy --staged --timeout 20 {swap_environment_name}')
     run('git reset HEAD', stdout=subprocess.DEVNULL)
+
+    # 새로 생성한 Environment의 LoadBalancer
+    swap_load_balancer = client.describe_environment_resources(
+        EnvironmentName=swap_environment_name
+    )['EnvironmentResources']['LoadBalancers'][0]
+
+    # 새로 생성한 LoadBalancer의 TargetGroup
+    swap_target_group = elb_client.describe_target_groups(
+        LoadBalancerArn=swap_load_balancer['Name'],
+    )['TargetGroups'][0]['TargetGroupArn']
+
+    # 새로 생성한 LoadBalancer의 SecurityGroup
+    swap_security_group_id = elb_client.describe_load_balancers(
+        LoadBalancerArns=[swap_load_balancer['Name']]
+    )['LoadBalancers'][0]['SecurityGroups'][0]
+
+    # 새 LoadBalancer에 HTTPS Listener추가 및 ACM추가
+    elb_client.create_listener(
+        LoadBalancerArn=swap_load_balancer['Name'],
+        Protocol='HTTPS',
+        Port=443,
+        Certificates=[
+            {
+                'CertificateArn': SECRETS['AWS_ACM_ARN'],
+            },
+        ],
+        DefaultActions=[
+            {
+                'Type': 'forward',
+                'TargetGroupArn': swap_target_group,
+            }
+        ]
+    )
+
+    # 새 LoadBalancer의 securityGroup에 443Port Inbound추가
+    ec2_client.authorize_security_group_ingress(
+        GroupId=swap_security_group_id,
+        IpPermissions=[
+            {
+                'FromPort': 443,
+                'IpProtocol': 'tcp',
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}],
+                'Ipv6Ranges': [{'CidrIpv6': '::/0'}],
+                'ToPort': 443,
+            }
+        ]
+    )
+
+    # CName Swap
+    client.swap_environment_cnames(
+        SourceEnvironmentName=environment_name,
+        DestinationEnvironmentName=swap_environment_name,
+    )
+
+    # 새 Environment의 CNAME이 CNAME_PREFIX로 시작할때까지 기다림
+    while True:
+        cname = client.describe_environments(
+            EnvironmentNames=[swap_environment_name],
+        )['Environments'][0]['CNAME']
+        sleep(5)
+        if CNAME_PREFIX in cname:
+            break
+
+    # 기존 Environment terminate
+    client.terminate_environment(
+        EnvironmentName=environment_name,
+    )
+    print('완료')
