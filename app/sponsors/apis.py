d9b151cdaf37831305889c93a9b373e8d318a56f
lhy <dev@lhy.kr>
2020-08-22 20:29:03 +0900

    sponsors앱의 API들을 viewset으로 전환

--- app/sponsors/apis.py
+++ app/sponsors/apis.py
@@ -1,5 +1,3 @@
-from django.utils.decorators import method_decorator
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
 from rest_framework.exceptions import ValidationError
 
@@ -7,14 +5,9 @@ from .filters import SponsorTierFilterSet
 from .models import SponsorTier
 from .serializers import SponsorTierDetailSerializer
 
+__all__ = ("SponsorTierListAPIView",)
 
-@method_decorator(
-    name="get",
-    decorator=swagger_auto_schema(
-        operation_summary="SponsorTier List",
-        operation_description="스폰서 등급(List), 스폰서 목록 포함",
-    ),
-)
+
 class SponsorTierListAPIView(generics.ListAPIView):
     queryset = SponsorTier.objects.prefetch_related("sponsor_set",)
     serializer_class = SponsorTierDetailSerializer
--- app/sponsors/urls.py
+++ app/sponsors/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from django_aid.drf.drf_yasg import schema
 
 from . import apis
 
 app_name = "sponsors"
 urlpatterns = [
-    path("tiers/", apis.SponsorTierListAPIView.as_view()),
+    path(
+        "tiers/",
+        schema(
+            apis.SponsorTierListAPIView,
+            [("get", {"operation_description": "스폰서 등급 목록(등급별 스폰서 목록 포함)"},)],
+        ).as_view(),
+    ),
 ]
