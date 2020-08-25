0aa9ffece56965412fa7812b6f120ecb1ced4866
lhy <dev@lhy.kr>
2020-08-25 18:33:02 +0900

    배포 스크립트 update

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-git fetch --all
-git pull
-sudo cp -f .scripts/ssl_renew.sh /etc/cron.monthly
-sudo chmod +x /etc/cron.monthly/ssl_renew.sh
-docker-compose build
-docker-compose stop
-docker-compose up --force-recreate --remove-orphans -d
-docker system prune -a --volumes -f
---
+++ update.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("branch")
+args = parser.parse_args()
+
+
+def run(cmd, **kwargs):
+    subprocess.run(cmd, shell=True, **kwargs)
+
+
+if __name__ == "__main__":
+    branch = args.branch
+    run(f"git fetch --all")
+    run(f"git checkout {branch}")
+    run(f"git pull origin {branch}")
+    run(f"sudo cp -f .scripts/ssl_renew.sh /etc/cron.monthly")
+    run(f"docker-compose build django_{branch}")
+    run(f"docker-compose stop django_{branch}")
+    run(f"docker-compose up --force-recreate --remove-orphans -d django_{branch}")
+    run(f"docker system prune -a --volumes -f")
+    print("Update complete")
