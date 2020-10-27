f158eaae65a1484e24c20de79f5f8a119d2ccb2c
lhy <dev@lhy.kr>
2020-10-27 16:55:57 +0900

    SSL docker run시 환경변수 설정 오류 수정

--- .scripts/ssl_cert.sh
+++ .scripts/ssl_cert.sh
@@ -1,6 +1,6 @@
-docker run --rm -it \
-      --env AWS_ACCESS_KEY_ID='$AWS_ROUTE53_ACCESS_KEY_ID' \
-      --env AWS_SECRET_ACCESS_KEY='$AWS_ROUTE53_SECRET_ACCESS_KEY' \
+export $(cat .env | xargs) && docker run --rm -it \
+      --env AWS_ACCESS_KEY_ID=$(echo "$AWS_ROUTE53_ACCESS_KEY_ID") \
+      --env AWS_SECRET_ACCESS_KEY=$(echo "$AWS_ROUTE53_SECRET_ACCESS_KEY") \
       -v '/etc/letsencrypt:/etc/letsencrypt' \
       -v '/var/lib/letsencrypt:/var/lib/letsencrypt' \
       certbot/dns-route53 certonly --dns-route53 \
